1ee3e983124a42b27b043667e500b3554ca63ce9
Hugo Sereno Ferreira <bytter@gmail.com>
2021-06-18 17:50:26 +0100

    Code cleanup; Use Blink-no-arduino.hex

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const mem = new Uint8Array(0xFFFF)
 mem.fill(0)
 const cpu = new avrcpu(mem)
 
-readIntelHex('./hexdumps/Blink-PB5.hex', mem)
+readIntelHex('./hexdumps/Blink-no-arduino.hex', mem)
 
-cpu.pc = 0x124
+cpu.pc = 0x080
 for (let i = 0; i < 8000; i+=1) cpu.step(false) 
